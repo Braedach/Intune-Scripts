835d39264a40329c485ef7e8fd1ce6da5947e321
Braedach <40484379+Braedach@users.noreply.github.com>
2021-12-18 08:59:00 +0800

    Add files via upload
    Initial code - very simple

---
+++ Windows Firewall Connection.ps1
@@ -0,0 +1,24 @@
+﻿<#
+
+    AUDIT FIREWALL POLICIES ENABLE FOR MSDE REPORTING
+
+    References:
+        https://docs.microsoft.com/en-us/microsoft-365/security/defender-endpoint/host-firewall-reporting?view=o365-worldwide
+        https://docs.microsoft.com/en-us/windows/win32/fwp/auditing-and-logging
+        https://security.microsoft.com/firewall?viewid=firewallInbound
+
+#>
+
+
+
+try
+    {
+    auditpol /set /subcategory:"Filtering Platform Packet Drop" /failure:enable
+    auditpol /set /subcategory:"Filtering Platform Connection" /failure:enable
+    Write-Host "Firewall reporting set" -ForegroundColor Green
+    }
+
+catch
+    {
+    Write-Host "Firewall reporting policy failed" -ForegroundColor Red
+    }
