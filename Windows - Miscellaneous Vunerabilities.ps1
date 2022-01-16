540b1ea4d843a792a8526298bcc48e0013526860
Braedach <40484379+Braedach@users.noreply.github.com>
2022-01-16 14:04:28 +0800

    Add files via upload
    Initial upload and deployed via test tenant.  Greenbone vulnerability assessment will see if its remediated or not.  Have my doubts

---
+++ Windows - Miscellaneous Vunerabilities.ps1
@@ -0,0 +1,28 @@
+﻿<#
+
+    Remove Miscellaneous vunerabilities from the system in accordance with the following
+
+    Reference:
+        NESSUS Vunerability scanning
+        Greenbone Vunerability scanning
+
+    Created 16-01-2022
+
+
+
+
+#>
+
+
+
+try
+    {  
+    netsh int tcp set global timestamps=disabled
+    }
+
+catch
+    {
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+    }
